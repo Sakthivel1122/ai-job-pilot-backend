2b43dde9b7007b37d931605d7f2716fb19178dc4
Sakthivel1122 <asakthivel1122@gmail.com>
2026-02-09 13:00:59 +0530

    resume analysis api changes

--- app/routers/resume.py
+++ app/routers/resume.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, HTTPException, status, Depends, UploadFile, File, Form, Query
 from app.dependencies.auth import get_current_user, role_required
 from typing import Optional
-from app.services.resume import get_suggestion_for_resume, get_job_application_resumes
+from app.services.resume import get_suggestion_for_resume, get_job_application_resumes, resume_analyzer_get_suggestion
 from app.utils.response import response
 from fastapi.encoders import jsonable_encoder
 
@@ -9,18 +9,42 @@ router = APIRouter(prefix="/api/v1")
 
 @router.post("/upload-resume")
 async def upload_resume(
-    resume: UploadFile = File(...),
+    resume: Optional[UploadFile] = File(None),
     job_description: Optional[str] = Form(""),
     job_application_id: str = Form(""),
     resume_name: str= Form(""),
+    resume_content_type: str= Form(""),
+    resume_text_content: Optional[str]= Form(""),
     current_user: dict = Depends(role_required("user")),
 ):
+    
     result = await get_suggestion_for_resume(
         resume, 
         job_description, 
         current_user, 
         job_application_id, 
         resume_name,
+        resume_content_type,
+        resume_text_content
+    )
+    return result
+
+@router.post("/get-resume-analysis")
+async def get_resume_analysis(
+    resume: Optional[UploadFile] = File(None),
+    job_description: Optional[str] = Form(""),
+    job_application_id: str = Form(""),
+    resume_content_type: str= Form(""),
+    resume_text_content: Optional[str]= Form(""),
+    current_user: dict = Depends(role_required("user")),
+):
+    
+    result = await resume_analyzer_get_suggestion(
+        resume=resume,
+        job_description=job_description,
+        job_application_id=job_application_id,
+        resume_content_type=resume_content_type,
+        resume_text_content=resume_text_content,
     )
     return result
 
--- app/services/resume.py
+++ app/services/resume.py
@@ -9,11 +9,13 @@ from typing import Optional
 from beanie import PydanticObjectId
 
 async def get_suggestion_for_resume(
-    resume: UploadFile = File(...),
+    resume: Optional[UploadFile] = File(None),
     job_description: Optional[str] = Form(""),
     current_user: User = None,
     job_application_id: str = "",
-    resume_name: str = Form("")
+    resume_name: Optional[str] = Form(""),
+    resume_content_type: str = Form(""),
+    resume_text_content: Optional[str] = Form("")
     # file_type: str = ""
 ):
     try:
@@ -25,10 +27,12 @@ async def get_suggestion_for_resume(
         if not job_application:
             return response(None, "Job Application Not Found", 400)
         
-        resume_bytes = await resume.read()
-
-        # Use your existing PDF extractor
-        resume_text = await extract_text_from_pdf(resume_bytes)
+        if (resume_content_type == "file"):
+            resume_bytes = await resume.read()
+            # Use your existing PDF extractor
+            resume_text = await extract_text_from_pdf(resume_bytes)
+        else:
+            resume_text = resume_text_content
 
         if job_description != "":
             job_description_text = job_description
@@ -53,6 +57,35 @@ async def get_suggestion_for_resume(
         await resume.insert()
         
         return response(result, "Resume Uploaded Successfully!", 200)
+    except Exception as e:
+        return response({'error': str(e)}, "API Failed", 400)
+
+async def resume_analyzer_get_suggestion(
+    resume: Optional[UploadFile] = File(None),
+    job_description: Optional[str] = Form(""),
+    job_application_id: str = "",
+    resume_content_type: str = Form(""),
+    resume_text_content: Optional[str] = Form(""),
+):
+    try:
+        if job_application_id:
+            job_application = await JobApplication.find_one(
+                JobApplication.id == ObjectId(job_application_id),
+                JobApplication.deleted_at == None
+            )
+            job_description_text = job_application.job_description
+        else:
+            job_description_text = job_description
+        
+        if resume_content_type == "file":
+            resume_bytes = await resume.read()
+            resume_text = await extract_text_from_pdf(resume_bytes)
+        else:
+            resume_text = resume_text_content
+
+        result = await get_ai_suggestion_for_resume(resume_text, job_description_text)
+
+        return response(result, "Resume Analyzed Successfully!", 200)
     except Exception as e:
         return response({'error': str(e)}, "API Failed", 400)
 
